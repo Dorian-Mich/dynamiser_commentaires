ab2f298627629372bef905a217a131bfcf01c0ef
Pakonten <dorian.michaud74@gmail.com>
2024-04-10 19:02:19 +0200

    mise au propre du code mais toujours pas fonctionnel

--- app.js
+++ app.js
@@ -4,14 +4,34 @@ const erreur = document.getElementById("error-message");
 
 let container = document.getElementById("comment-list");
 let div = document.createElement("div");
-
 let prenom_input = document.getElementById("first-name").value;
-
 let prenom = document.createTextNode(prenom_input);
 
 let commentaire_input = document.getElementById("message").value;
 
-function valider() {
+let ajout_commentaire = function (prenom_input) {
+  let contenu_html = (div.innerHTML =
+    `
+  <div class="flex-1 py-10 border-t border-gray-200">
+    <h3 class="font-medium text-gray-900">` +
+    prenom_input +
+    `</h3>
+    <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, enim
+        vitae quasi culpa est, excepturi cum voluptatum exercitationem id,
+        deleniti similique maiores! Totam, itaque praesentium quod ipsa quos
+        ratione et.
+      </p>
+    </div>
+  </div>`);
+
+  //ajouter le commentaire dans la liste
+  container.appendChild(div);
+  div.appendChild(contenu_html);
+};
+
+let erreur_check = function () {
   //empecher le chargement de la page
   event.preventDefault();
 
@@ -22,28 +42,12 @@ function valider() {
   } else {
     //cacher le message d'erreur
     erreur.style.display = "none";
-
-    let contenu_html = (div.innerHTML =
-      `
-  <div class="flex-1 py-10 border-t border-gray-200">
-    <h3 class="font-medium text-gray-900">` +
-      prenom +
-      `</h3>
-    <div class="prose prose-sm mt-4 max-w-none text-gray-500">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, enim
-        vitae quasi culpa est, excepturi cum voluptatum exercitationem id,
-        deleniti similique maiores! Totam, itaque praesentium quod ipsa quos
-        ratione et.
-      </p>
-    </div>
-  </div>
-`);
-
-    //ajouter le commentaire dans la liste
-    container.appendChild(div);
-    div.appendChild(contenu_html);
   }
+};
+
+function valider() {
+  erreur_check();
+  ajout_commentaire(prenom_input);
 }
 
 boutton.addEventListener("click", valider);
